2fcffde7147474285ba9cba8b7fd87545e02ea32
Tunahan Yıldız <tunahanyildiz@outlook.it>
2022-10-09 22:39:45 +0300

    Admin teması eklendi giriş ekranı ve generic repository

---
+++ InternInformation/Controllers/loginController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternInformation.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
